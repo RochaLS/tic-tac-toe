7a21dd50e8a4f62d2c211e1e147775085f25eac8
Lucas Rocha <ld.rocha@hotmail.com>
2020-12-07 18:33:34 -0800

    completed project

--- game.js
+++ game.js
@@ -1,32 +1,44 @@
 // Gets all the DOM objects
 const DOM = (() => {
     const squares = document.querySelectorAll('.square');
-    const startGameBtn = document.querySelector('.startBtn');
+    const restartBtn = document.querySelector('.restartBtn');
     const gameContainer = document.querySelector('.gameContainer');
+    const message = document.querySelector('.message');
 
-    return { startGameBtn, squares, gameContainer };
+    const showAlert = (message) => {
+        alert(`${message}`);
+    }
+
+    return { squares, gameContainer, showAlert , restartBtn, message };
 })();
+
+const PlayerFactory = (player, choice) => {
+    return { player, choice };
+};
 
 // Builds the board and players and deals with game logic
 const GameBoard = (() =>  {
     const container = DOM.gameContainer;
-    const startButton = DOM.startGameBtn;
+    const restartButton = DOM.restartBtn;
     const winConditions = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]];
     let players = [];
     let gameBoard = [];
     let roundCounter = 1;
+    let gameOver = false;
     
-    startButton.addEventListener('click', () => {
-        players.push(PlayerFactory('p1', 'X'), PlayerFactory('p2', 'O'));
-        renderBoard();
-        startButton.disabled = true;
-    });
+    
+    players.push(PlayerFactory('p1', 'X'), PlayerFactory('p2', 'O'));
+    renderBoard();
+   
+
+    restartButton.addEventListener('click', clear);
 
     function renderBoard() {
         for (let i = 0; i < 9; i++) {
             const squareView = document.createElement('div');
-            squareView.style.backgroundColor = 'blue';
-            squareView.textContent = ''
+            squareView.style.backgroundColor = '#9147FF';
+            squareView.style.borderRadius = '5px';
+            squareView.textContent = '';
             squareView.style.display = 'flex';
             squareView.style.justifyContent = 'center';
             squareView.style.alignItems = 'center';
@@ -41,28 +53,58 @@ const GameBoard = (() =>  {
     }
 
     function updateSquare(square) {
-        if (roundCounter % 2  != 0 && square.textContent == '') {
+        if (roundCounter % 2  != 0 && square.textContent == '' && gameOver == false) {
             // player1 turn
             square.textContent = 'X';
             roundCounter++;
-            checkForWinner();
-        } else if (square.textContent == '') {
+            checkForWinnerP1();
+        } else if (square.textContent == '' && roundCounter % 2 == 0 && gameOver == false) {
             // player2 turn
             square.textContent = 'O';
             roundCounter++;
-            checkForWinner();
+            checkForWinnerP2();
+        }
+
+        // Check if board is full
+        if (roundCounter > 9) {
+            displayMessage(`It's a Tie!`);
         }
     }
 
-    function checkForWinner() {
-        //
+    function checkForWinnerP1() {
+        winConditions.forEach(element => {
+            if (gameBoard[element[0]].textContent == 'X' && gameBoard[element[1]].textContent == 'X' && gameBoard[element[2]].textContent == 'X') {
+                displayMessage('Player 1 Wins!');
+                gameOver = true;
+            }
+        });
     }
-    return { gameBoard, players };
 
+    function checkForWinnerP2() {
+        winConditions.forEach(element => {
+            if (gameBoard[element[0]].textContent == 'O' && gameBoard[element[1]].textContent == 'O' && gameBoard[element[2]].textContent == 'O') {
+                displayMessage('Player 2 Wins!');
+                gameOver = true;
+            }
+        });
+    }
+
+    function clear() {
+        for (let i = 0; i < gameBoard.length; i++) {
+            const element = gameBoard[i];
+            element.textContent = '';
+        }
+        roundCounter = 1;
+        gameOver = false;
+        DOM.message.style.display = 'none';
+    }
+
+    function displayMessage(message) {
+        DOM.message.textContent = message;
+        DOM.message.style.display = 'block';
+    }
 })();
 
 
-const PlayerFactory = (player, choice, myTurn) => {
-    return { player, choice };
-};
+
 
